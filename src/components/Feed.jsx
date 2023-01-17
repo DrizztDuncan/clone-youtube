83956f10e1eec0d1d9faf96aac4c66ff8df1c5bc
Duncan <duncantheinvictus@gmail.com>
2023-01-17 20:57:12 +0800

    fetch

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import { fetchFromAPI } from "../utils/FetchFromAPI";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -14,7 +20,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography
           className="copyright"
@@ -26,7 +35,8 @@ const Feed = () => {
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2}>
-          New<span style={{ color: "#F31503" }}>videos</span>
+          {selectedCategory}
+          <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
         <Videos videos={[]} />
