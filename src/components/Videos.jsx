28e48b6b929542ae69f663ad088d4809f3204824
Duncan <duncantheinvictus@gmail.com>
2023-01-31 20:56:32 +0800

    if videoId exist shows BOX

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -12,12 +12,15 @@ const Videos = ({ videos, direction }) => {
       justifyContent="start"
       gap={2}
     >
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
-      ))}
+      {videos.map(
+        (item, idx) =>
+          item.id.videoId != null && (
+            <Box key={idx}>
+              {item.id.videoId && <VideoCard video={item} />}
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+          )
+      )}
     </Stack>
   );
 };
